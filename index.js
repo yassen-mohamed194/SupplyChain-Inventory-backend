39c905d5c93b6672474eb026d9d13197928614ab
Abdallah Wael <bedowael365@gmail.com>
2026-04-18 00:06:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:5672']
+  })
+);
 app.use(express.json());
 
 // Health Check
